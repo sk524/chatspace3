252be37def522bd1b874b1891a5abe6576220c47
sk524 <58733794+sk524@users.noreply.github.com>
2020-01-19 21:18:20 +0900

    side-bar commit
    サイドバーのヘッダーを作りました

--- db/migrate/20200112074427_create_groups.rb
+++
@@ -1,8 +0,0 @@
-class CreateGroups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :groups do |t|
-      t.string :name, :text, null: false
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20200112075548_add_image_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddImageToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :mwssage, :image, :string
-    add_index :message, :image, unique: true
-  end
-end
