15a188de98195d6b98393c8839b7366b07e9c643
sk524 <58733794+sk524@users.noreply.github.com>
2020-02-01 18:37:03 +0900

    db commit
    Since db migration was not done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200130103804) do
+ActiveRecord::Schema.define(version: 20200201091505) do
 
   create_table "group_userrails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "g"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 20200130103804) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
