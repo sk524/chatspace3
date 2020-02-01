a19ec7abe529bc8bd5c26b02472971516986a155
sk524 <58733794+sk524@users.noreply.github.com>
2020-02-01 19:08:38 +0900

    massega commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root  'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, onry: [:index, :new, :create, :edit, :update]
+  resources :groups, onry: [:new, :create, :edit, :update]
+    resources :messages, onry: [:index, :create]
 end
