dc7ab814cd692860803ae3814746c59aa053ae10
sk524 <58733794+sk524@users.noreply.github.com>
2020-02-02 15:26:10 +0900

    group 2 commit
    Create message model
    Configure routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'messages#index'
+  root  'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, onry: [:new, :create, :edit, :update]
+  resources :groups, onry: [:new, :create, :edit, :update] do
     resources :messages, onry: [:index, :create]
+end
 end
